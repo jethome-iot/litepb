f8663347d49b8a66345aa36c23233b37882b0490
Pavel Sokolov <pavel@tvip.ru>
2025-10-06 19:20:38 +0300

    Move exmaple test to examples folder

---
+++ examples/cpp/rpc/litepb_rpc/test/unity_config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Enable support for double-precision assertions in Unity
+#define UNITY_INCLUDE_DOUBLE
+#define UNITY_DOUBLE_TYPE double
+#define UNITY_DOUBLE_PRECISION 1e-9
+
+// Enable 64-bit integer assertions (int64_t / uint64_t)
+#define UNITY_SUPPORT_64
